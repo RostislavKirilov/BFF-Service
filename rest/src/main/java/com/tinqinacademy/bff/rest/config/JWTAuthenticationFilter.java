4b750b51cb5e2debe1ef2541cd57fb0e8c5cbf91
RostislavKirilov <rosti048@gmail.com>
2024-08-22 13:29:08 +0300

    Fixed bugs, added authentication to some methods.

--- rest/src/main/java/com/tinqinacademy/bff/rest/config/JWTAuthenticationFilter.java
+++ rest/src/main/java/com/tinqinacademy/bff/rest/config/JWTAuthenticationFilter.java
@@ -1,11 +1,17 @@
 package com.tinqinacademy.bff.rest.config;
 
+import com.tinqinacademy.authentication.api.getrole.input.GetRoleInput;
+import com.tinqinacademy.authentication.api.getrole.output.GetRoleOutput;
+import com.tinqinacademy.authentication.api.getusername.input.GetUsernameInput;
+import com.tinqinacademy.authentication.api.getusername.output.GetUsernameOutput;
+import com.tinqinacademy.authentication.api.operations.validatetoken.input.ValidateTokenInput;
 import com.tinqinacademy.authentication.restexport.RestExportValidateToken;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -23,19 +29,22 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
     private final RestExportValidateToken restExportValidateToken;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal ( @NotNull HttpServletRequest request, @NotNull HttpServletResponse response,
+                                      @NotNull FilterChain filterChain ) throws ServletException, IOException {
 
         String token = getTokenFromRequest(request);
+        ValidateTokenInput input = ValidateTokenInput.builder().token(token).build();
 
-        if (StringUtils.hasText(token) && restExportValidateToken.validateToken(token)) {
+        if (StringUtils.hasText(token) && restExportValidateToken.validateToken(input)) {
 
-            String username = restExportValidateToken.getUsernameFromToken(token);
-            String role = restExportValidateToken.getRoleFromToken(token);
+            GetUsernameInput getUsernameInput = GetUsernameInput.builder().token(token).build();
+            GetRoleInput getRoleInput = GetRoleInput.builder().token(token).build();
+           GetUsernameOutput username = restExportValidateToken.getUsernameFromToken(getUsernameInput);
+            GetRoleOutput role = restExportValidateToken.getRoleFromToken(getRoleInput);
 
             CustomUser userDetails = CustomUser.builder()
-                    .username(username)
-                    .role(role)
+                    .username(username.getUsername())
+                    .role(role.getRole())
                     .build();
 
             UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
@@ -49,7 +58,7 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-    private String getTokenFromRequest(HttpServletRequest request) {
+    private String getTokenFromRequest ( HttpServletRequest request ) {
         String bearerToken = request.getHeader("Authorization");
 
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
